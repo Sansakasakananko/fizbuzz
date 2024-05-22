1eff44d3eb7f3ae034424509550174f698380484
sansakasakananoko <misaka.seiko@gmail.com>
2024-05-22 16:43:12 +0900

    ２－２編集

--- ruby2/ruby2_2.rb
+++ ruby2/ruby2_2.rb
@@ -3,16 +3,8 @@ b = 3
 sum = a + b
 puts sum
 
-a = 8
-b = 3
-sum = a + b
-puts suma = 8
-b = 3
-sum = a + b
-puts suma = 8
-b = 3
-sum = a + b
-puts suma = 8
-b = 3
-sum = a + b
-puts sum
+
+
+test
+
+test
