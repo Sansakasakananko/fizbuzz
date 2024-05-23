28a0ca3b9f9e42b1ec6985cf9a8d71b9b7975649
sansakasakananoko <misaka.seiko@gmail.com>
2024-05-23 11:55:36 +0900

    Your commit message

---
+++ main.rb
@@ -0,0 +1,7 @@
+# main.rb
+
+require_relative './fizbuzz2'
+
+1.upto(100) do |n|
+  puts fizbuz(n)
+end
---
+++ test.rb
@@ -0,0 +1,34 @@
+# require_relative './fizbazz'
+
+# def check(input, expected)
+#     actual = fizbuzz(input)
+#     unless actual == expected
+#         abort "テストに失敗しました。\n入力値: #{input}, 期待する出力: #{expected}, 実際の出力: #{actual}"
+#     end
+# end
+
+# check(1, 1)
+# check(3, 'Fizz')
+# check(5, 'Buzz')
+# check(15, 'FizzBuzz')
+
+# puts 'テストに成功しました'
+
+
+
+
+require_relative './fizbuzz2'
+
+def check(input, expected)
+    actual = fizzbuzz(input)
+    unless actual == expected
+        abort "テストに失敗しました。\n入力値: #{input}, 期待する出力: #{expected}, 実際の出力: #{actual}"
+    end
+end
+
+check(1, 1)
+check(3, 'Fizz')
+check(5, 'Buzz')
+check(15, 'FizzBuzz')
+
+puts 'テストに成功しました'
